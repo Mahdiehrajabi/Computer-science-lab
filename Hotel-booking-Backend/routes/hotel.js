861e76ff68b46491d53647011c06a181ba3d3932
Mahdiehrajabi <133880837+Mahdiehrajabi@users.noreply.github.com>
2024-12-19 17:58:09 +0100

    hotel.js

--- Hotel-booking-Backend/routes/hotel.js
+++ Hotel-booking-Backend/routes/hotel.js
@@ -1,8 +1,8 @@
 const express = require('express');
-const Hotel = require('../models/Hotel'); // مدل هتل را وارد کنید
+const Hotel = require('../models/Hotel'); 
 const router = express.Router();
 
-// مسیر POST برای ایجاد هتل جدید
+
 router.post('/', async (req, res) => {
     try {
         const hotel = await Hotel.create(req.body);
@@ -12,7 +12,7 @@ router.post('/', async (req, res) => {
     }
 });
 
-// اضافه کردن مسیرهای دیگر به دلخواه (مثلاً برای دریافت هتل‌ها)
+
 router.get('/', async (req, res) => {
     try {
         const hotels = await Hotel.findAll();
