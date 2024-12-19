4758640053fc0b6461e7e98593327ea51bb54ffe
Mahdiehrajabi <133880837+Mahdiehrajabi@users.noreply.github.com>
2024-12-19 17:49:53 +0100

    app.js

--- Hotel-booking-Backend/app.js
+++ Hotel-booking-Backend/app.js
@@ -11,7 +11,7 @@ dotenv.config();
 
 const app = express();
 
-// تنظیمات CORS
+//  CORS
 app.use(cors({
     origin: 'http://localhost:3000', // آدرس فرانت‌اند (بدون `/` در انتها)
     methods: ['GET', 'POST', 'PUT', 'DELETE'], // متدهای مجاز
@@ -20,16 +20,15 @@ app.use(cors({
 
 app.options('*', cors()); // مدیریت preflight requests
 
-// پردازش JSON
+//  JSON
 app.use(express.json());
 
-// مسیرهای API
+//  API
 app.use('/auth', authRoutes);
 app.use('/hotels', hotelRoutes);
 app.use('/rooms', roomRoutes);
 app.use('/bookings', bookingRoutes);
 
-// اتصال به پایگاه داده
 sequelize.authenticate()
     .then(() => console.log('Connected to the database'))
     .catch((err) => console.error('Unable to connect to the database:', err));
@@ -38,7 +37,7 @@ sequelize.sync()
     .then(() => console.log('Database & tables created!'))
     .catch((err) => console.error('Error syncing database:', err));
 
-// راه‌اندازی سرور
+// 
 const PORT = 3001;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
