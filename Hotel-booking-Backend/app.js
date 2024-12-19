1cc2999595ed6f6955728632cd45d5aed6455d63
Mahdiehrajabi <133880837+Mahdiehrajabi@users.noreply.github.com>
2024-12-19 17:51:37 +0100

    Update app.js

--- Hotel-booking-Backend/app.js
+++ Hotel-booking-Backend/app.js
@@ -18,7 +18,7 @@ app.use(cors({
     allowedHeaders: ['Content-Type', 'Authorization'], 
 }));
 
-app.options('*', cors()); // مدیریت preflight requests
+app.options('*', cors()); 
 
 //  JSON
 app.use(express.json());
